08cf33a70c8d9fe84e6ee68d2a2bb49054b108a0
Franz Geffke <mail@gofranz.com>
2026-01-30 20:10:27 +0700

    px: ollama added

--- px/packages/ai.scm
+++ px/packages/ai.scm
@@ -2,7 +2,11 @@
 ;;; Copyright © 2026 Franz Geffke <mail@gofranz.com>
 
 (define-module (px packages ai)
+  #:use-module ((guix licenses) #:prefix license:)
+  #:use-module (gnu packages base)
   #:use-module (gnu packages bash)
+  #:use-module (gnu packages compression)
+  #:use-module (gnu packages gcc)
   #:use-module (guix download)
   #:use-module (guix gexp)
   #:use-module (guix packages)
@@ -63,3 +67,44 @@ exec ~a \"$@\"
 It can understand your codebase, edit files, run terminal commands, and
 handle entire workflows.  This package disables auto-updates.")
     (license (nonfree "https://code.claude.com/docs/en/legal-and-compliance"))))
+
+(define-public ollama
+  (package
+    (name "ollama")
+    (version "0.15.2")
+    (source
+     (origin
+       (method url-fetch)
+       (uri (string-append
+             "https://github.com/ollama/ollama/releases/download/v"
+             version "/ollama-linux-amd64.tar.zst"))
+       (sha256
+        (base32 "0avf2m7b58ps2xy3jc41l7njq2xnxfmfb69cwicc35rvrbiswps4"))))
+    (build-system binary-build-system)
+    (arguments
+     (list
+      #:strip-binaries? #f
+      #:validate-runpath? #f
+      #:patchelf-plan
+      #~'(("bin/ollama" ("glibc" "gcc")))
+      #:install-plan
+      #~'(("bin/ollama" "bin/"))
+      #:phases
+      #~(modify-phases %standard-phases
+          (replace 'unpack
+            (lambda* (#:key inputs #:allow-other-keys)
+              (invoke "tar" "--use-compress-program=zstd" "-xf"
+                      (assoc-ref inputs "source")))))))
+    (native-inputs
+     (list zstd))
+    (inputs
+     (list glibc
+           `(,gcc "lib")))
+    (supported-systems '("x86_64-linux"))
+    (home-page "https://ollama.com")
+    (synopsis "Run large language models locally")
+    (description
+     "Ollama allows you to run large language models locally.
+It provides a simple API for creating, running and managing models,
+as well as a library of pre-built models that can be easily used.")
+    (license license:expat)))
