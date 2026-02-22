589a9ae4d8c3568a24e26c6d5f0ec9142199926f
Franz Geffke <mail@gofranz.com>
2026-02-22 07:32:56 +0000

    px: voxtype: Update to v0.6.2

--- px/packages/audio.scm
+++ px/packages/audio.scm
@@ -161,7 +161,7 @@ minimize its security impact.")
 (define-public voxtype
   (package
     (name "voxtype")
-    (version "0.6.1")
+    (version "0.6.2")
     (source
      (origin
        (method git-fetch)
@@ -170,7 +170,7 @@ minimize its security impact.")
              (commit (string-append "v" version))))
        (file-name (git-file-name name version))
        (sha256
-        (base32 "124ln8qly1s5z766npbaanznrqhq50m7mdp219dabfx0fbim8965"))
+        (base32 "1rd2gdwyn1a4q84qwsi2yf8nxsdfcjlxlwldw0nyklnvyy4w1pq7"))
        (snippet
         #~(begin
             (use-modules (guix build utils))
