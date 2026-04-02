466c044683334fc676a2a1c436691703b915e001
Franz Geffke <mail@gofranz.com>
2026-04-02 21:35:41 +0100

    px: zed: Update to v0.230.0

--- px/packages/editors.scm
+++ px/packages/editors.scm
@@ -447,7 +447,7 @@ predictive code completion, and integrations with development workflows.")
 (define-public zed
   (package
     (name "zed")
-    (version "0.229.0")
+    (version "0.230.0")
     (source #f)
     (build-system binary-build-system)
     (arguments
@@ -509,7 +509,7 @@ predictive code completion, and integrations with development workflows.")
                         "https://github.com/zed-industries/zed/releases/download/v"
                         version "/zed-linux-x86_64.tar.gz"))
                   (sha256
-                   (base32 "1jq8m5qi0f3h3rzqa7jlck6gy2r7mp53x3k40qzklldayzm4s3qq"))))
+                   (base32 "037dr65k4si4iz4sk72z9ylj4wrigzr7d9k08h5pl9dsw9rsq837"))))
               ("bash-minimal" ,bash-minimal)
               ("glib" ,glib)
               ("gtk+" ,gtk+)
