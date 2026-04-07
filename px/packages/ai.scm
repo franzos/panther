f4a13521f39eaf881fe0ddd49b71a3f2cda398c7
Franz Geffke <mail@gofranz.com>
2026-04-07 13:14:08 +0100

    px: ollama: Update to v0.20.3

--- px/packages/ai.scm
+++ px/packages/ai.scm
@@ -75,7 +75,7 @@ handle entire workflows.  This package disables auto-updates.")
 (define-public ollama
   (package
     (name "ollama")
-    (version "0.19.0")
+    (version "0.20.3")
     (source
      (origin
        (method url-fetch)
@@ -83,7 +83,7 @@ handle entire workflows.  This package disables auto-updates.")
              "https://github.com/ollama/ollama/releases/download/v"
              version "/ollama-linux-amd64.tar.zst"))
        (sha256
-        (base32 "1lm54ssmcnrhr7yhfb5xwwis22j3ymgvk8a30l1r3rb9lb1vxv5k"))))
+        (base32 "0zh6qwb6iw77ca49i6xn0p8z7z938cf6fqwx4ra3r2wnvrh80wyn"))))
     (build-system binary-build-system)
     (arguments
      (list
