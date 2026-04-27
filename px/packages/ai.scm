17b957a7c50bed68a0de1e0bac20ed621b045669
Franz Geffke <mail@gofranz.com>
2026-04-27 08:14:04 +0100

    px: tku: Update to v0.1.11

--- px/packages/ai.scm
+++ px/packages/ai.scm
@@ -116,7 +116,7 @@ as well as a library of pre-built models that can be easily used.")
 (define-public tku
   (package
     (name "tku")
-    (version "0.1.10")
+    (version "0.1.11")
     (source
      (origin
        (method git-fetch)
@@ -125,7 +125,7 @@ as well as a library of pre-built models that can be easily used.")
              (commit (string-append "v" version))))
        (file-name (git-file-name name version))
        (sha256
-        (base32 "0ndmba6aci48laycyv6jzlngibviwa8qmsn5r95xzjxlla93a5q2"))))
+        (base32 "0a8xy9alqpq2g5lza0jvrscs8xpdazkl2c58h3sjs9laiqdvn4x2"))))
     (build-system cargo-build-system)
     (arguments
      (list
