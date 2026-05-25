881f2fee1888e87280f3e74efc68e60088f55edc
Franz Geffke <mail@gofranz.com>
2026-05-25 20:03:22 +0100

    px: zed: Fix stale hash for re-uploaded v1.3.6

--- px/packages/editors.scm
+++ px/packages/editors.scm
@@ -509,7 +509,7 @@ predictive code completion, and integrations with development workflows.")
                         "https://github.com/zed-industries/zed/releases/download/v"
                         version "/zed-linux-x86_64.tar.gz"))
                   (sha256
-                   (base32 "0g9blwb27m44mmg0cp3wcrayh6jxkxnf5yq5bhyqw99y9fwy6i71"))))
+                   (base32 "0rqblragcrpa224r7ymbgkp1wd90r4axpaj1ljmdg7k8kli2q5aw"))))
               ("bash-minimal" ,bash-minimal)
               ("glib" ,glib)
               ("gtk+" ,gtk+)
