c173251c5994006272e53f20bf34402a49254892
Franz Geffke <mail@gofranz.com>
2026-05-26 06:50:59 +0100

    px: atril-thumbnailer: Fix build with gexp-style arguments

--- px/packages/atril-thumbnailer.scm
+++ px/packages/atril-thumbnailer.scm
@@ -4,6 +4,7 @@
 (define-module (px packages atril-thumbnailer)
   #:use-module (guix packages)
   #:use-module (guix utils)
+  #:use-module (guix gexp)
   #:use-module (gnu packages mate))
 
 (define-public atril-thumbnailer
@@ -23,17 +24,15 @@
     (arguments
      (substitute-keyword-arguments (package-arguments atril)
        ((#:phases phases)
-        `(modify-phases ,phases
-           (replace 'build
-             (lambda _
-               (invoke "make" "thumbnailer") #t))
-           (replace 'install
-             (lambda _
-               (invoke "make" "install" "thumbnailer") #t))
-           (add-after 'install 'remove-desktop-file
-             (lambda* (#:key outputs #:allow-other-keys)
-               (let ((out (assoc-ref outputs "out")))
-                 (invoke "rm"
-                         (string-append out
-                                        "/share/applications/atril.desktop"))
-                 #t)))))))))
+        #~(modify-phases #$phases
+            (replace 'build
+              (lambda _
+                (invoke "make" "thumbnailer")))
+            (replace 'install
+              (lambda _
+                (invoke "make" "install" "thumbnailer")))
+            (add-after 'install 'remove-desktop-file
+              (lambda* (#:key outputs #:allow-other-keys)
+                (delete-file
+                 (string-append (assoc-ref outputs "out")
+                                "/share/applications/atril.desktop"))))))))))
