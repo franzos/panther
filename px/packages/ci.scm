e662716009cb5d7da87b544ebfc7df1b79b97259
Franz Geffke <mail@gofranz.com>
2026-06-02 15:46:36 +0100

    px: gitlab-runner: Update to v19.0.1

--- px/packages/ci.scm
+++ px/packages/ci.scm
@@ -14,14 +14,14 @@
 (define-public gitlab-runner
   (package
     (name "gitlab-runner")
-    (version "v19.0.0")
+    (version "v19.0.1")
     (source
      (origin
        (method url-fetch)
        (uri (string-append "https://gitlab-runner-downloads.s3.amazonaws.com/"
              version "/binaries/gitlab-runner-linux-amd64"))
        (sha256
-        (base32 "1zqmnmzgspaaxyxzghkcb7iby3vignpif8ih7c1pj36ry8n3halg"))))
+        (base32 "1dljb8frisqr7p7vdwgvhn53hxzhknyfpgr2q1x7c2llmzmjnalp"))))
     (build-system trivial-build-system)
     (supported-systems '("x86_64-linux" "i686-linux")) ;; As long as we download binaries
     (arguments
