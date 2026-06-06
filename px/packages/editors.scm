601ca1fdf45dac882d984b475da8e7ab9c3d321d
Franz Geffke <mail@gofranz.com>
2026-06-06 09:44:59 +0100

    px: zed: Update to v1.5.4

--- px/packages/editors.scm
+++ px/packages/editors.scm
@@ -447,7 +447,7 @@ predictive code completion, and integrations with development workflows.")
 (define-public zed
   (package
     (name "zed")
-    (version "1.5.3")
+    (version "1.5.4")
     (source #f)
     (build-system binary-build-system)
     (arguments
@@ -509,7 +509,7 @@ predictive code completion, and integrations with development workflows.")
                         "https://github.com/zed-industries/zed/releases/download/v"
                         version "/zed-linux-x86_64.tar.gz"))
                   (sha256
-                   (base32 "1555imwra3x210bs833siqnbrkkp9wsh86nqbwf6x8w38imw6gpx"))))
+                   (base32 "06y3nx7kaj9w0cbrp6qxwx5f6f5wq9vdjv0cav2xvssw5dwfddyd"))))
               ("bash-minimal" ,bash-minimal)
               ("glib" ,glib)
               ("gtk+" ,gtk+)
