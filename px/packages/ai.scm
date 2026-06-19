53094bde5ac8a9d7da9318284d188df05b4d1068
Franz Geffke <mail@gofranz.com>
2026-06-19 14:09:36 +0100

    px: claude-code: Update to v2.1.183

--- px/packages/ai.scm
+++ px/packages/ai.scm
@@ -21,7 +21,7 @@
 (define-public claude-code
   (package
     (name "claude-code")
-    (version "2.1.181")
+    (version "2.1.183")
     (source
      (origin
        (method url-fetch)
@@ -30,7 +30,7 @@
              "86c565f3-f756-42ad-8dfa-d59b1c096819/claude-code-releases/"
              version "/linux-x64/claude"))
        (sha256
-        (base32 "172f6b8hjyakssndx5syp7mbz67h7frgh1afpb89aqx8v7lx9zrm"))))
+        (base32 "1kndm3q2dhrkyzkyzgn130pcpg8xh5j1zs2y5ksrsa95bff40fyz"))))
     (build-system binary-build-system)
     (arguments
      (list
