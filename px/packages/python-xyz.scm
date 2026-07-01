e54c2561b53aba249475a487ce5558de911de464
Franz Geffke <mail@gofranz.com>
2026-07-01 01:31:57 +0100

    px: djlint: Update to v1.39.6

--- px/packages/python-xyz.scm
+++ px/packages/python-xyz.scm
@@ -475,13 +475,13 @@ the CSS counterpart to jsbeautifier.")
 (define-public djlint
   (package
     (name "djlint")
-    (version "1.39.2")
+    (version "1.39.6")
     (source
      (origin
        (method url-fetch)
        (uri (pypi-uri "djlint" version))
        (sha256
-        (base32 "0d4bm8gc79bipwx5bcijs3fislfbdwmfq9nscyhy1l96pl1yy9yx"))))
+        (base32 "1n5wpimzw00gxdk9yikncq4ygps6rj8s9l70z2p8vy7l8j323z9a"))))
     (build-system pyproject-build-system)
     (arguments
      (list
