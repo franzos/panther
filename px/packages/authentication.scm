9e43b91349fd7d2bd1bad6b6bc32968d23767b72
Franz Geffke <mail@gofranz.com>
2026-07-05 08:18:32 +0100

    px: keycloak: Update to v26.6.4

--- px/packages/authentication.scm
+++ px/packages/authentication.scm
@@ -26,7 +26,7 @@
 (define-public keycloak
   (package
     (name "keycloak")
-    (version "26.6.3")
+    (version "26.6.4")
     (source
      (origin
        (method url-fetch)
@@ -34,7 +34,7 @@
              "https://github.com/keycloak/keycloak/releases/download/"
              version "/keycloak-" version ".tar.gz"))
        (sha256
-        (base32 "0i4f47ys07j1i0dpmlycic2r0ns5as99mpqbswybi40iri2d08fv"))))
+        (base32 "13rffwk157y3wm5sqh94mnb8h8bgrzjl6p17dqi2fmd0prmmcsrq"))))
     (build-system copy-build-system)
     (arguments
      (list
