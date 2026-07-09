083bf04e2087b177671bb5e33f55179cf8f6f41c
Franz Geffke <mail@gofranz.com>
2026-07-09 23:55:15 +0100

    px: qtutilities: Update to v6.21.3

--- px/packages/qt.scm
+++ px/packages/qt.scm
@@ -151,7 +151,7 @@
 (define-public qtutilities
   (package
     (name "qtutilities")
-    (version "6.21.2")
+    (version "6.21.3")
     (source
      (origin
        (method url-fetch)
@@ -159,7 +159,7 @@
              "https://github.com/Martchus/qtutilities/archive/refs/tags/v"
              version ".tar.gz"))
        (sha256
-        (base32 "1rcgrwk7fz8129cmb2fpfxnffzz5mjng6ns1w5jpzjjl4wns9bg4"))))
+        (base32 "1f5jdb4r8ln7ia8mid7ch5kcy1zvyd494llfiajxzs7c30h0dzl4"))))
     (build-system cmake-build-system)
     (native-inputs (list cpputilities qtbase-5 qttools-5))
     (arguments
