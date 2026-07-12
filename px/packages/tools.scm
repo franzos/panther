cdbfbb77abcbf74f5380dd736288fdeafecb9785
Franz Geffke <mail@gofranz.com>
2026-07-12 07:59:48 +0100

    px: codex: Update to v0.144.1

--- px/packages/tools.scm
+++ px/packages/tools.scm
@@ -36,7 +36,7 @@
 (define-public codex
   (package
     (name "codex")
-    (version "0.144.0")
+    (version "0.144.1")
     (source
      (origin
        (method url-fetch)
@@ -49,8 +49,8 @@
        (sha256
         (base32
          (match (or (%current-system) (%current-target-system))
-           ("x86_64-linux" "1fy80pxm1fancrd33xzr71i22b6iyvns1ai9503z6jmb43y86n3j")
-           ("aarch64-linux" "0lbbrkn857nk5zlzy3lp271yfbpcqdx5zfzm8g3mbddxa1wlmi67"))))))
+           ("x86_64-linux" "0a2s9yyyphkq3iwgn2bnkkgzhzvxsldpvf8d2bacgz351ki1l2c4")
+           ("aarch64-linux" "1f5mm1flbsjv62iqix3z8nd2kvp34d16lxfkpd6ysv74k1gyzy5r"))))))
     (build-system binary-build-system)
     (arguments
      (list
