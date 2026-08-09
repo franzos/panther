97aa9b26f6afe2875be9755e64868c2979991a3f
Franz Geffke <mail@gofranz.com>
2026-08-09 21:02:19 +0100

    px: noctalia-beta: Added

--- px/packages/wm.scm
+++ px/packages/wm.scm
@@ -8,17 +8,36 @@
   #:use-module (guix gexp)
   #:use-module (guix utils)
   #:use-module (guix build-system copy)
+  #:use-module (guix build-system meson)
   #:use-module (gnu packages bash)
+  #:use-module (gnu packages calendar)
+  #:use-module (gnu packages cpp)
+  #:use-module (gnu packages crypto)
+  #:use-module (gnu packages curl)
+  #:use-module (gnu packages fontutils)
+  #:use-module (gnu packages freedesktop)
+  #:use-module (gnu packages gl)
   #:use-module (gnu packages glib)
+  #:use-module (gnu packages gnome)
+  #:use-module (gnu packages gtk)
   #:use-module (gnu packages hardware)
+  #:use-module (gnu packages image)
   #:use-module (gnu packages imagemagick)
+  #:use-module (gnu packages jemalloc)
   #:use-module (gnu packages linux)
+  #:use-module (gnu packages markup)
+  #:use-module (gnu packages maths)
+  #:use-module (gnu packages multiprecision)
+  #:use-module (gnu packages pkg-config)
   #:use-module (gnu packages polkit)
+  #:use-module (gnu packages pulseaudio)
   #:use-module (gnu packages python)
+  #:use-module (gnu packages stb)
   #:use-module (gnu packages vulkan)
   #:use-module (gnu packages wget)
   #:use-module (gnu packages window-management)
-  #:use-module (gnu packages xdisorg))
+  #:use-module (gnu packages xdisorg)
+  #:use-module (gnu packages xml))
 
 ;; Niri with SHM screencast support (PR #1791)
 ;; Fixes black screen in Chrome/WebRTC screen sharing
@@ -145,4 +164,77 @@ Quickshell (Qt/QML).  It provides a status bar, panels, application launcher,
 notifications, lock screen, idle management, OSD, theming, wallpaper
 management, desktop widgets, dock, and multi-monitor support.  Noctalia
 natively supports Niri, Hyprland, Sway, Scroll, Labwc and MangoWC.")
+    (license license:expat)))
+
+;; Noctalia v5: a from-scratch C++ rewrite dropping Qt/Quickshell in favour of
+;; Wayland + OpenGL ES.  Still beta, hence packaged alongside noctalia-shell.
+(define-public noctalia-beta
+  (package
+    (name "noctalia-beta")
+    (version "5.0.0-beta.7")
+    (source
+     (origin
+       (method git-fetch)
+       (uri (git-reference
+             (url "https://github.com/noctalia-dev/noctalia")
+             (commit (string-append "v" version))))
+       (file-name (git-file-name name version))
+       (sha256
+        (base32 "06knh7g9vzp65dz80x7pb9rwgih6hh0vyxi0ymk5a35nihs4j6gm"))))
+    (build-system meson-build-system)
+    (arguments
+     (list
+      #:build-type "release"
+      #:phases
+      #~(modify-phases %standard-phases
+          (add-after 'unpack 'prepare-for-build
+            (lambda _
+              ;; /bin/sh doesn't exist in the build environment.
+              (substitute* "tests/process_test.cpp"
+                (("/bin/(sh)" _ cmd) (which cmd)))
+              ;; Guix installs the STB headers without the "stb/" prefix.
+              (substitute* (find-files "." "\\.cpp$|^meson\\.build$")
+                (("\\bstb/stb_") "stb_")))))))
+    (native-inputs (list pkg-config))
+    (inputs
+     (list cairo
+           curl
+           fontconfig
+           freetype
+           glib
+           gmp
+           harfbuzz
+           jemalloc
+           (librsvg-for-system)
+           libical
+           libjxl
+           libqalculate
+           libsecret
+           libsndfile
+           libsodium
+           libwebp
+           libxkbcommon
+           libxml2
+           linux-pam
+           md4c
+           mesa
+           mpfr
+           nlohmann-json
+           pango
+           pipewire
+           polkit
+           sdbus-c++
+           stb-image-resize2
+           stb-image-write
+           tomlplusplus
+           wayland
+           wayland-protocols
+           wireplumber))
+    (home-page "https://noctalia.dev/")
+    (synopsis "Sleek, customizable desktop shell crafted for Wayland")
+    (description
+     "Noctalia is a desktop shell for Wayland compositors, providing bars,
+dock, application launcher, notifications, lock screen, wallpaper management
+and settings.  This is the version 5 rewrite, built directly on Wayland and
+OpenGL ES with no Qt or GTK toolkit dependency.")
     (license license:expat)))
