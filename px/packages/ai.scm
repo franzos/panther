49ce0c7a142c168cf7c0a0f5bd8ff5ca85e056e1
Franz Geffke <mail@gofranz.com>
2026-08-10 11:53:23 +0100

    px: claude-desktop: Update to v1.26832.0

--- px/packages/ai.scm
+++ px/packages/ai.scm
@@ -76,7 +76,7 @@ handle entire workflows.  This package disables auto-updates.")
 (define-public claude-desktop
   (package
     (name "claude-desktop")
-    (version "1.25927.0")
+    (version "1.26832.0")
     (source
      (origin
        (method url-fetch)
@@ -85,7 +85,7 @@ handle entire workflows.  This package disables auto-updates.")
              "main/c/claude-desktop/claude-desktop_" version "_amd64.deb"))
        (file-name (string-append name "-" version ".deb"))
        (sha256
-        (base32 "0rlwlrqp2wslq4f0d4hz2s25c9skq73lw4kb1glcpl9fr1zd4qr3"))))
+        (base32 "1ms5092l51lld5w7m54gz4z3kgrzyn6i5qcnfqq3pd4v23ag1iib"))))
     (build-system chromium-binary-build-system)
     (arguments
      (list
