3c8d4c8ce38c832e4daf08eeb201318c1544ecdc
Franz Geffke <mail@gofranz.com>
2026-08-24 10:16:12 +0100

    px: claude-code: Update to v2.1.241

--- px/packages/ai.scm
+++ px/packages/ai.scm
@@ -24,7 +24,7 @@
 (define-public claude-code
   (package
     (name "claude-code")
-    (version "2.1.237")
+    (version "2.1.241")
     (source
      (origin
        (method url-fetch)
@@ -33,7 +33,7 @@
              "86c565f3-f756-42ad-8dfa-d59b1c096819/claude-code-releases/"
              version "/linux-x64/claude"))
        (sha256
-        (base32 "05irzfmk91s58z9gbgkf8nwfnmqng2a4jqfndz7r71hhy1km35vk"))))
+        (base32 "1f1pf2yb7k0xr3w49wbqa223c6lyabv9j17wh5jvg0pzdj3bsw87"))))
     (build-system binary-build-system)
     (arguments
      (list
