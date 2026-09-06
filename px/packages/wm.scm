bb653e71e092babfe4a220838dfa04bab50066cd
Franz Geffke <mail@gofranz.com>
2026-09-06 13:11:43 +0100

    px: noctalia-5: Update to v5.0.1

--- px/packages/wm.scm
+++ px/packages/wm.scm
@@ -174,7 +174,7 @@ C++ rewrite.")
 (define-public noctalia-5
   (package
     (name "noctalia-5")
-    (version "5.0.0-beta.10")
+    (version "5.0.1")
     (source
      (origin
        (method git-fetch)
@@ -183,7 +183,7 @@ C++ rewrite.")
              (commit (string-append "v" version))))
        (file-name (git-file-name name version))
        (sha256
-        (base32 "03s76i5gl62lnzawk8ld4ld74n8vn35crgzp7g6cb3bghfwc8a2s"))))
+        (base32 "0f07y0jglkx1jxkcsm90d9149wpk3lxz3zqig8mg5dvbmxpvf93n"))))
     (build-system meson-build-system)
     (arguments
      (list
